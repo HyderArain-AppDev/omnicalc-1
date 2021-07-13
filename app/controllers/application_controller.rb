5011c469d57a56f758a9bdf3d7226c3c51942bda
hyderarain <harain@chicagobooth.edu>
2021-07-13 21:52:06 +0000

    sqrt working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,12 @@ class ApplicationController < ActionController::Base
         @square = @number * @number
         render({ :template => "calc_templates/square_results.html.erb", })
     end
+    def blank_square_root_form
+        render({ :template => "calc_templates/square_root_form.html.erb", })
+    end
+    def result_square_root_form
+        @number = params.fetch("number").to_f
+        @sqrt = Math.sqrt(@number)
+        render({ :template => "calc_templates/square_root_results.html.erb", })
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
 
 get("/square/new", {:controller => "application", :action => "blank_square_form"})
 get("/square/results", {:controller => "application", :action => "result_square_form"})
-
+get("/square_root/new", {:controller => "application", :action => "blank_square_root_form"})
+get("/square_root/results", {:controller => "application", :action => "result_square_root_form"})
 end

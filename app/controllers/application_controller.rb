28aad2b87376a0386d4b73b04c048aab05976745
hyderarain <harain@chicagobooth.edu>
2021-07-13 22:15:38 +0000

    random funcion es magnifico

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,4 +15,16 @@ class ApplicationController < ActionController::Base
         @sqrt = Math.sqrt(@number)
         render({ :template => "calc_templates/square_root_results.html.erb", })
     end
+
+    def blank_random_form
+        render({ :template => "calc_templates/random_form.html.erb", })
+    end
+
+    def result_random_form
+        @min = params.fetch("user_min").to_f
+        @max = params.fetch("user_max").to_f
+        @rand = rand(@min..@max.to_f)
+        render({ :template => "calc_templates/random_results.html.erb"})
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ get("/square/new", {:controller => "application", :action => "blank_square_form"
 get("/square/results", {:controller => "application", :action => "result_square_form"})
 get("/square_root/new", {:controller => "application", :action => "blank_square_root_form"})
 get("/square_root/results", {:controller => "application", :action => "result_square_root_form"})
+
+get("/random/new", {:controller => "application", :action => "blank_random_form"})
+get("/random/results", {:controller => "application", :action => "result_random_form"})
 end
